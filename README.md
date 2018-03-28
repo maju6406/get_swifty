9d026bb0e11605357db83e1deb0de06b594613d4
maju6406 <beersy@gmail.com>
2018-03-28 14:51:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# get_swifty
+
+Simple proof of concept of a Puppet Task written in Swift. 
---
+++ tasks/init.swift
@@ -0,0 +1,12 @@
+#!/usr/bin/swift
+
+import Foundation 
+class HelloWorld { 
+    # Swift 2+ code
+    func print() { 
+        Swift.print("Hello world!") 
+    } 
+} 
+
+let hw = HelloWorld()
+hw.print()
